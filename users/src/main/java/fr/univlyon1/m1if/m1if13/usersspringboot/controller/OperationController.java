f2bcedc2dbf27cba5872f924b8a76af3056dafcb
Océane <oceane.jousselme@etu.univ-lyon1.fr>
2021-02-09 20:22:04 +0100

    Login, logout and JWT

--- users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/OperationController.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/OperationController.java
@@ -1,15 +1,28 @@
 package fr.univlyon1.m1if.m1if13.usersspringboot.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import fr.univlyon1.m1if.m1if13.usersspringboot.DAO.UserDao;
+import fr.univlyon1.m1if.m1if13.usersspringboot.model.User;
+import fr.univlyon1.m1if.m1if13.usersspringboot.utils.JWTHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.naming.AuthenticationException;
+import java.util.Map;
 
 @Controller
 public class OperationController {
 
     // TODO récupérer le DAO...
+    @Autowired
     UserDao users;
+    JWTHelper jwt;
 
     /**
      * Procédure de login "simple" d'un utilisateur
@@ -18,16 +31,38 @@ public class OperationController {
      * @return Une ResponseEntity avec le JWT dans le header "Authentication" si le login s'est bien passé, et le code de statut approprié (204, 401 ou 404).
      */
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestParam("login") String login, @RequestParam("password") String password, @RequestHeader("Origin") String origin) {
+    public ResponseEntity<Void> login(@RequestParam("login") String login, @RequestParam("password") String password, @RequestHeader("Origin") String origin) throws AuthenticationException {
         // TODO
-        return null;
+        Boolean userExists = users.get(login).isPresent();
+
+        if(userExists) {
+            User user = users.get(login).get();
+
+            try {
+                user.authenticate(password);
+                String token = jwt.generateToken(user.getLogin(), origin);
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("Authentication", token);
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).headers(headers).build();
+            } catch (AuthenticationException e) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     /**
      * Réalise la déconnexion
      */
     @DeleteMapping("/logout")
-    // TODO
+    public ResponseEntity<Void> logout(@RequestParam("token") String token) {
+        DecodedJWT jwt = JWT.decode(token);
+        String loginToDeco = jwt.getClaim("login").asString();
+        users.get(loginToDeco).get().disconnect();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
     /**
      * Méthode destinée au serveur Node pour valider l'authentification d'un utilisateur.
@@ -38,6 +73,14 @@ public class OperationController {
     @GetMapping("/authenticate")
     public ResponseEntity<Void> authenticate(@RequestParam("token") String token, @RequestParam("origin") String origin) {
         // TODO
-        return null;
+        String verify = jwt.verifyToken(token, origin);
+
+        if(verify.equals("Not valid")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        } else if (verify.equals("Bad token")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
     }
 }
---
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/utils/JWTHelper.java
@@ -0,0 +1,67 @@
+package fr.univlyon1.m1if.m1if13.usersspringboot.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * Classe qui centralise les opérations de validation et de génération d'un token "métier", c'est-à-dire dédié à cette application.
+ *
+ * @author Lionel Médini
+ */
+public class JWTHelper {
+    private static final String SECRET = "monbeausecret";
+    private static final String ISSUER = "Spring App";
+    private static final long LIFETIME = 1800000; // Durée de vie d'un token : 30 secondes
+    private static final Algorithm algorithm = Algorithm.HMAC256(SECRET);
+
+    /**
+     * Vérifie l'authentification d'un utilisateur grâce à un token JWT
+     *
+     * @param token le token à vérifier
+     * @param origin   la requête HTTP (nécessaire pour vérifier si l'origine de la requête est la même que celle du token
+     * @return un booléen qui indique si le token est bien formé et valide (pas expiré) et si l'utilisateur est authentifié
+     */
+    public static String verifyToken(String token, @NotNull String origin) throws NullPointerException, JWTVerificationException {
+        JWTVerifier authenticationVerifier = JWT.require(algorithm)
+                .withIssuer(ISSUER)
+                .withAudience(origin) // Non-reusable verifier instance
+                .build();
+        try {
+            authenticationVerifier.verify(token); // Lève une NullPointerException si le token n'existe pas, et une JWTVerificationException s'il est invalide
+            DecodedJWT jwt = JWT.decode(token); // Pourrait lever une JWTDecodeException mais comme le token est vérifié avant, cela ne devrait pas arriver
+            return jwt.getClaim("sub").asString();
+        } catch (Exception e) {
+            if(e instanceof JWTVerificationException) {
+                return "Not valid";
+            } else {
+                return "Bad token";
+            }
+        }
+
+    }
+
+    /**
+     * Crée un token avec les caractéristiques de l'utilisateur
+     *
+     * @param login le login de l'utilisateur
+     * @param origin     la requête HTTP pour pouvoir en extraire l'origine avec getOrigin()
+     * @return le token signé
+     * @throws JWTCreationException si les paramètres ne permettent pas de créer un token
+     */
+    public static String generateToken(String login, String origin) throws JWTCreationException {
+        return JWT.create()
+                .withIssuer(ISSUER)
+                .withSubject(login)
+                .withAudience(origin)
+                .withExpiresAt(new Date(new Date().getTime() + LIFETIME))
+                .sign(algorithm);
+    }
+
+}
+
