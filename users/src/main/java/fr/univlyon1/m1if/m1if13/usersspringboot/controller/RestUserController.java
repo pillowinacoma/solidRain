5950eca601c81193b2891aa0110d9fb5bcbd2d2f
Aziz <abdelazizsbaai@gmail.com>
2021-03-08 17:58:37 +0100

    CORS with simple client

--- users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/RestUserController.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/RestUserController.java
@@ -51,6 +51,7 @@ public class RestUserController {
             @ApiResponse(responseCode = "404", description = "utilisateur non trouvé", content = @Content)})
     @GetMapping(path = "/users/{login}",
             produces = {"application/json", "application/xml"})
+    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:80", "http://localhost:8080", "http://localhost" , "http://192.168.75.9" , "https://192.168.75.9"})
     public ResponseEntity<Object> getUser(@Parameter(description = "the login of the user") @PathVariable(name = "login") String userLogin) {
         ResponseEntity<Object> responseEntity = null;
         try {
@@ -73,6 +74,7 @@ public class RestUserController {
                             @Content(mediaType = "text/html")}) })
     @GetMapping(path = "/users",
             produces = {"application/json", "application/xml"})
+    @CrossOrigin(origins = {"http://localhost" , "http://192.168.75.9" , "https://192.168.75.9"})
     public ResponseEntity<Object> getUsers() {
         ResponseEntity<Object> res = null;
         Set<User> setOfUsers = userDAO.get();
