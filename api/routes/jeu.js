6b0d7aa4373b06e4ae22c500bc73fc46f6864851
Océane <oceane.jousselme@etu.univ-lyon1.fr>
2021-03-09 18:28:34 +0100

    Mise en place squelette express

---
+++ api/routes/jeu.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var router = express.Router()
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+// define the home page route
+router.get('/resources', function (req, res) {
+  res.send('Birds home page')
+})
+module.exports = router
