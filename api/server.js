2d697268413fdda06d164bf633b7ad8427b59e84
Océane <oceane.jousselme@etu.univ-lyon1.fr>
2021-03-09 17:55:39 +0100

    TP3 - 2.1 : Static et gestion des erreurs

--- api/server.js
+++ api/server.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.send('Hello world !!');
 });
 
+app.get('/static', (req, res) => {
+    app.use(express.static('public'));
+});
+
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
 });
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
