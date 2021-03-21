b17d6b95a723ec12d9eb0d542d2b1623b6e70a68
Aziz <abdelazizsbaai@gmail.com>
2021-03-21 19:34:31 +0100

    gitlab-ci

--- users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/UsersApplication.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/UsersApplication.java
@@ -2,10 +2,11 @@ package fr.univlyon1.m1if.m1if13.usersspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class UsersApplication {
+public class UsersApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
